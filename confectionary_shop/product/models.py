ce38648f4bb74d72b0276a60bcf82045f02cb1be
amjad ahmadi <amjadahmadi883@gmail.com>
2022-12-14 16:46:56 +0330

    create serialzers for view category an add product

--- confectionary_shop/product/models.py
+++ confectionary_shop/product/models.py
@@ -7,6 +7,7 @@ from Cake_designing.models import Cake_Designing
 from customer.models import User
 from core.models import BaseModel
 
+
 # Create your models here.
 
 
@@ -23,18 +24,29 @@ class Products(BaseModel):
     img = models.ImageField()
     description = models.TextField()
 
-
-class Stock(BaseModel):
-    product = models.OneToOneField(Products, on_delete=models.CASCADE)
-    kilo = models.FloatField(null=True)
-    count = models.IntegerField(null=True)
-    price = models.FloatField()
-    after_discount = models.FloatField(null=True)
+    def __str__(self):
+        return f"{self.product_name}({self.description})"
 
 
 class Category(BaseModel):
     category_name = models.CharField(max_length=100)
-    parent_category = models.ForeignKey('self', on_delete=models.CASCADE)
+    img = models.ImageField(default='draw1.png', null=True, blank=True)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Stock(BaseModel):
+    product = models.OneToOneField(Products, on_delete=models.CASCADE)
+    kilo = models.FloatField(null=True, blank=True)
+    count = models.IntegerField(null=True, blank=True)
+    price = models.FloatField()
+    after_discount = models.FloatField(null=True, blank=True)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.product.product_name
 
 
 class Discount(BaseModel):
--- confectionary_shop/product/urls.py
+++ confectionary_shop/product/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
-from .views import Detail_product
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import Detail_product, CategoryList
 from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = 'product'
 urlpatterns = [
                   path('detail/', Detail_product.as_view(), name='detail'),
+                  path('category_list/', CategoryList.as_view(), name='category_list'),
+
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
