d0c9a40fbff5dbec4113d97a036b0914b70100c3
amjad ahmadi <amjadahmadi883@gmail.com>
2022-12-25 12:29:11 +0330

    create API view for feeling and taste model

--- confectionary_shop/Cake_designing/urls.py
+++ confectionary_shop/Cake_designing/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CakeDesigning
+from .views import CakeDesigning, FeelingAndTasteAPI
 from django.conf import settings
 from django.conf.urls.static import static
 import django.utils.translation as e
@@ -7,5 +7,6 @@ import django.utils.translation as e
 app_name = 'cake_designing'
 urlpatterns = [
                   path('cake_designing/', CakeDesigning.as_view(), name='cake_designing'),
+                  path('get_feeling_taste/<feeling_id>/<taste_id>', FeelingAndTasteAPI.as_view(), name='feeling_and_taste_api'),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- confectionary_shop/Cake_designing/views.py
+++ confectionary_shop/Cake_designing/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import CakeDesigningForm
+from rest_framework.views import APIView
+from .models import Feeling, Taste
+from rest_framework.response import Response
+from .serializers import FeelingSerializers, TasteSerializers
 
 
 # Create your views here.
@@ -10,3 +14,15 @@ class CakeDesigning(View):
         f = CakeDesigningForm()
         print(f)
         return render(request, 'Cake_designing/cake_design.html', {'form': f})
+
+
+class FeelingAndTasteAPI(APIView):
+    def get(self, request, feeling_id, taste_id):
+        feeling = Feeling.objects.get(id=feeling_id)
+        taste = Taste.objects.get(id=taste_id)
+        feeling_serial = FeelingSerializers(feeling)
+        taste_serial = TasteSerializers(taste)
+        f = feeling_serial.data
+        t = taste_serial.data
+
+        return Response({'feeling':f, 'taste':t})
