2fb23097a48ce90e738a8b0795eb21dbee94767c
amjad ahmadi <amjadahmadi883@gmail.com>
2023-01-01 21:51:05 +0330

    create url for discount code view

--- confectionary_shop/product/urls.py
+++ confectionary_shop/product/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
-
-from .views import Detail_product, CategoryList, ProductListAPI
+from rest_framework.routers import DefaultRouter
+from .views import Detail_product, CategoryList, ProductListAPI, DiscountCode
 from django.conf import settings
 from django.conf.urls.static import static
-
+router = DefaultRouter()
+router.register(r'discount_code', DiscountCode,basename="discount_code")
 app_name = 'product'
 urlpatterns = [
                   path('detail/<pk>', Detail_product.as_view(), name='detail'),
                   path('category_list/', CategoryList.as_view(), name='category_list'),
                   path('product_list/<category_id>', ProductListAPI.as_view(), name='product_list'),
+                  path('', include(router.urls)),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
