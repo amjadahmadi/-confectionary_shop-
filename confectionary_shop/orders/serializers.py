d22ac7adddb28573c0e3ea093ea4ce856f6df7f8
amjad ahmadi <amjadahmadi883@gmail.com>
2023-01-04 10:38:47 +0330

    override def create for check the warehouse stock

--- confectionary_shop/orders/serializers.py
+++ confectionary_shop/orders/serializers.py
@@ -12,3 +12,27 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order_Item
         fields = '__all__'
+
+    def create(self, validated_data):
+        stock = validated_data['stock_id']
+        count = stock.count
+        kilo = stock.kilo
+        count_per_kilo = (kilo / count) if (count and kilo) else None
+
+        if validated_data['kilo']:
+            if kilo - validated_data['amount'] < 0:
+                validated_data['order_status'] = 'CA'
+            else:
+                stock.kilo -= validated_data['amount']
+                if count_per_kilo:
+                    stock.count -= validated_data['amount'] / count_per_kilo
+
+        else:
+            if count - validated_data['amount'] < 0:
+                validated_data['order_status'] = 'CA'
+            else:
+                stock.count -= validated_data['amount']
+                if count_per_kilo:
+                    stock.kilo -= validated_data['amount'] * count_per_kilo
+        stock.save()
+        return super(OrderItemSerializer, self).create(validated_data)
