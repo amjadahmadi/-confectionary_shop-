11fe3dc279d177854cd9890e4d379e3d0b8a4536
amjad ahmadi <amjadahmadi883@gmail.com>
2023-01-06 12:04:35 +0330

    create profile and user serializer

--- confectionary_shop/customer/serializers.py
+++ confectionary_shop/customer/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
-from .models import Addresses
+from .models import Addresses, Profile, User
 
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Addresses
         fields = '__all__'
+        read_only_fields = ('user',)
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
